63feaec748270465a065dd5754cb5cc0fbb63bb0
Trey Tacon <ttacon@gmail.com>
2016-08-19 10:42:32 -0400

    Bulkify CloudWatch metric sending

--- index.js
+++ index.js
@@ -44,7 +44,38 @@
  * Then, whenever we want to publish a metric, we simply do:
  *
  * ```
- * myMetric.put(value, metric, additionalDimensions, done);
+ * myMetric.put(value, metric, additionalDimensions);
+ * ```
+ *
+ * Be aware that the `put` call does not actually send the metric to CloudWatch
+ * at that moment. Instead, it stores unsent metrics and sends them to
+ * CloudWatch on a predetermined interval (to help get around sending too many
+ * metrics at once - CloudWatch limits you by default to 150 put-metric data
+ * calls per second). The default interval is 5 seconds, if you want metrics
+ * sent at a different interva, then provide that option when construction your
+ * CloudWatch Metric:
+ *
+ * ```
+ * var myMetric = new cloudwatchMetrics.Metric('namespace', 'Count', [{
+ * 	Name: 'environment',
+ * 	Value: 'PROD'
+ * }], {
+ * 	sendInterval: 3 * 1000 // It's specified in milliseconds.
+ * });
+ * ```
+ *
+ * You can also register a callback to be called when we actually send metrics
+ * to CloudWatch - this can be useful for logging put-metric-data errors:
+ * ```
+ * var myMetric = new cloudwatchMetrics.Metric('namespace', 'Count', [{
+ * 	Name: 'environment',
+ * 	Value: 'PROD'
+ * }], {
+ * 	sendCallback: (err) => {
+ * 		if (!err) return;
+ * 		// Do your error handling here.
+ * 	}
+ * });
  * ```
  */
 
@@ -67,7 +98,9 @@ function initialize(config) {
 
 
 const DEFAULT_METRIC_OPTIONS = {
-  enabled: true
+  enabled: true,
+  sendInterval: 5000,
+  sendCallback: () => {}
 };
 
 /**
@@ -92,6 +125,11 @@ function Metric(namespace, units, defaultDimensions, options) {
   this.units = units;
   this.defaultDimensions = defaultDimensions || [];
   this.options = _.defaults(options || {}, DEFAULT_METRIC_OPTIONS);
+  this._storedMetrics = [];
+
+  if (this.options.enabled) {
+    setInterval(this._sendMetrics, this.options.sendInterval);
+  }
 }
 
 /**
@@ -100,27 +138,38 @@ function Metric(namespace, units, defaultDimensions, options) {
  * @param {String} namespace            Name of the metric
  * @param {Array} additionalDimensions  Array of additional CloudWatch metric dimensions. See
  * http://docs.aws.amazon.com/AmazonCloudWatch/latest/APIReference/API_Dimension.html for details.
- * @param {function} done               Node style callback
  */
-Metric.prototype.put = function(value, metricName, additionalDimensions, done) {
-  // Only publish if we are enabled
+Metric.prototype.put = function(value, metricName, additionalDimensions) {
   var self = this;
-  if (!self.options.enabled) {
-    process.nextTick(done);
-  } else {
+  // Only publish if we are enabled
+  if (self.options.enabled) {
     additionalDimensions = additionalDimensions || [];
-    var params = {
-      MetricData: [{
-        MetricName: metricName,
-        Dimensions: self.defaultDimensions.concat(additionalDimensions),
-        Unit: self.units,
-        Value: value
-      }],
-      Namespace: self.namespace
-    };
-
-    self.cloudwatch.putMetricData(params, done);
+    self._storedMetrics.push({
+      MetricName: metricName,
+      Dimensions: self.defaultDimensions.concat(additionalDimensions),
+      Unit: self.units,
+      Value: value
+    });
   }
+};
+
+/**
+ * _sendMetrics is called on a specified interval (defaults to 5 seconds but
+ * can be overridden but providing a `sendInterval` option when creating a
+ * Metric). It is what actually sends metrics to CloudWatch. It passes the
+ * sendCallback option (if provided) as the callback to the put-metric-data
+ * call. This can be useful for logging AWS errors.
+ */
+Metric.prototype._sendMetrics = function() {
+  var self = this;
+  // NOTE: this would be racy except that NodeJS is single threaded.
+  const dataPoints = self._storedMetrics;
+  self._storedMetrics = [];
+
+  self.cloudwatch.putMetricData({
+    MetricData: dataPoints,
+    Namespace: self.namespace
+  }, self.options.sendCallback);
 };
 
 module.exports = {
